4abdf055256c6951e0d3430f8f4985494be05c65
IHK_Office_Laptop <IHK_Office_Laptop>
2024-07-09 23:21:33 +0500

    attempting GGOGLE IDENTITY SERVICES

---
+++ lib/user_model.dart
@@ -0,0 +1,8 @@
+class AppUser {
+  final String uuid;
+  final String name;
+  final String email;
+   String? password;
+
+  AppUser({required this.uuid, required this.name, required this.email});
+}
