4d8921b4a153323237741c0d37dfa4f047e7093c
IHK_Office_Laptop <IHK_Office_Laptop>
2024-07-19 02:21:27 +0500

    UserAvatarChildOverlay Brushed up, but navigation from this widget pending

--- lib/widgets/user_avatar_overlay_child.dart
+++ lib/widgets/user_avatar_overlay_child.dart
@@ -9,27 +9,26 @@ class UserAvatarOverlayChild extends ConsumerStatefulWidget {
       _UserAvatarOverlayChildState();
 }
 
-class _UserAvatarOverlayChildState
-    extends ConsumerState<UserAvatarOverlayChild> with SingleTickerProviderStateMixin {
-   late AnimationController _animationController;
+class _UserAvatarOverlayChildState extends ConsumerState<UserAvatarOverlayChild>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
   late Animation<double> _animation;
- 
+
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 450),
       vsync: this,
       // animationBehavior: AnimationBehavior.preserve
     );
     // Modified line:
-    _animation =
-        Tween<double>(begin: 200, end: 250).animate(CurvedAnimation(
-          parent: _animationController,
-          // parent: _animationController,
-          curve: Curves.easeInQuad,
-        ));
-        _animationController.forward();
+    _animation = Tween<double>(begin: 250, end: 300).animate(CurvedAnimation(
+      parent: _animationController,
+      // parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    _animationController.forward();
   }
 
   @override
@@ -38,10 +37,11 @@ class _UserAvatarOverlayChildState
 
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     final profile = ref.watch(userProfileProvider);
-   
+
     return Positioned(
       top: 50,
       right: 120,
@@ -51,29 +51,109 @@ class _UserAvatarOverlayChildState
           // duration: Durations.medium1,
           builder: (context, child) => SizedBox(
             // Modified lines:
-            width: _animation.value*1.5, // Directly use animation value for width
-            height: _animation.value, // Directly use animation value for height
+            width: _animation.value *
+                1.5, // Directly use animation value for width
+            // height: _animation.value, // Directly use animation value for height
             child: child,
           ),
           child: Card(
-            elevation: 15,
+            elevation: 5,
             color: Colors.white,
             child: Center(
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                profilePic(),
                 Text(
                     'Cadet ID: ${data!.kitNo}'), // Display Kit Number as Cadet ID
                 Text('Name: ${data.name}'), // Display Name
                 Text('House: ${data.house}'), // Display House
-                if (data.roles
-                    .contains(UserRole.admin())) // Check for Admin Role
-                  TextButton.icon(
-                    onPressed: () {},
-                    label: Text('Admin Center'),
-                    iconAlignment: IconAlignment.end,
-                    icon: Icon(Icons.launch),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Card(
+                    color: Colors.blueGrey.shade50,
+                    elevation: 5,
+                    child: ListTile(
+                      leading: Icon(Icons.person, size: 40),
+
+                      title: Text('Account '),
+                      subtitle: Text('${data.email}'),
+                      // enabled: true,
+                      trailing: TextButton(
+                        onPressed: () {
+                          //TODO
+                        },
+                        child: Text(
+                          'SIGN OUT',
+                          style:
+                              Theme.of(context).textTheme.titleSmall?.copyWith(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                        ),
+                      ),
+                    ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Card(
+                    color: Colors.blueGrey.shade50,
+                    elevation: 5,
+                    child: ListTile(
+                      leading: Icon(Icons.person, size: 40),
+
+                      title: Text('Account  Type'),
+                      subtitle: Row(
+                        children: 
+                          profile.asData!.value!.roles.map(
+                            (e) => Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(),
+                                borderRadius: BorderRadius.circular(12),
+                                  
+                                ),
+                                
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                  child: Text(e.value),
+                                )), 
+                            ),
+                          ).toList(),
+                        
+                      ),
+                      // enabled: true,
+                      trailing: 
+                         (data.roles
+                    .contains(UserRole.admin()))? // Check for Admin Role
+                      TextButton(
+                        onPressed: () {
+                          //TODO
+                        },
+                        child: Text(
+                          'Admin Center',
+                          style:
+                              Theme.of(context).textTheme.titleSmall?.copyWith(
+                                    // color: Colors.red,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                        ),
+                      ):Container(),
+                    ),
+                  ),
+                ),
+
+                // if (data.roles
+                //     .contains(UserRole.admin())) // Check for Admin Role
+                //   TextButton.icon(
+                //     onPressed: () {},
+                //     label: const Text('Admin Center'),
+                //     iconAlignment: IconAlignment.end,
+                //     icon: const Icon(Icons.launch),
+                //   ),
+                SizedBox(height: 10),
               ],
             )),
           ),
@@ -81,6 +161,18 @@ class _UserAvatarOverlayChildState
         error: (error, stackTrace) =>
             Text(error.toString() + stackTrace.toString()),
         loading: () => const Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+
+  Padding profilePic() {
+    return const Padding(
+      padding: EdgeInsets.all(12.0),
+      child: CircleAvatar(
+        radius: 58,
+        backgroundColor: Color.fromARGB(255, 4, 45, 92),
+        backgroundImage: NetworkImage(
+            'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'),
       ),
     );
   }
