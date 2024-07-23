5fa1d9769c6c12e3508cbf6c6fd3076b68c78346
IHK_Office_Laptop <IHK_Office_Laptop>
2024-07-23 08:30:55 +0500

    a little bit more wiring up

--- lib/widgets/create_entry_stepper.dart
+++ lib/widgets/create_entry_stepper.dart
@@ -12,9 +12,47 @@ class CreateEntryStepper extends ConsumerStatefulWidget {
 
 class _CreateEntryStepperState extends ConsumerState<CreateEntryStepper> {
   final formKey_EntryDetails = GlobalKey<FormState>();
+
+  late TextEditingController entryNameController;
+  late TextEditingController entryNumberController;
+  late TextEditingController entryStrengthController;
+  late DateTime? selectedStartDate;
+  late DateTime? selectedEndDate;
+  late Function(DateTime) onStartDateChanged;
+  late Function(DateTime) onEndDateChanged;
+  late TextEditingController entryTitleController;
+  late TextEditingController entrySloganController;
+  late VoidCallback? onSave;
+  late VoidCallback? onNext;
+  late VoidCallback? onCancel;
   int stepIndex = 0;
 
-  get entryStrengthController => null;
+  // get entryStrengthController => null;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    entryNameController = TextEditingController();
+    entryNameController = TextEditingController();
+    entrySloganController = TextEditingController();
+    entryTitleController = TextEditingController();
+    entrySloganController = TextEditingController();
+    selectedStartDate = DateTime.now();
+    selectedEndDate = DateTime.now();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    entryNameController.dispose();
+    entryNameController.dispose();
+    entryNameController.dispose();
+    entrySloganController.dispose();
+    entryTitleController.dispose();
+    entrySloganController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,6 +110,13 @@ class _CreateEntryStepperState extends ConsumerState<CreateEntryStepper> {
               padding: const EdgeInsets.all(8.0),
               child: EntryDetailsForm(
                 formKeyEntryDetails: formKey_EntryDetails,
+                entryNameController: entryNameController,
+                entryNumberController: entryNameController,
+                entryStrengthController: entryStrengthController,
+                entrySloganController: entrySloganController,
+                entryTitleController: entryTitleController,
+                selectedEndDate: selectedEndDate,
+                selectedStartDate: selectedStartDate,
                 onCancel: () {},
                 onSave: () {},
                 onNext: () {},
@@ -92,11 +137,32 @@ class _CreateEntryStepperState extends ConsumerState<CreateEntryStepper> {
       ),
     );
   }
-
+ 
   void _saveEntryToFirestore() {
     if (formKey_EntryDetails.currentState!.validate()) {
-      print('******************************** ENTRY FORM VALID*****************************');
-      try
+      // final docRef =
+      print(
+          '******************************** ENTRY FORM VALID*****************************');
+      var entryData = {
+        'entryName': entryNameController.text,
+        'entryNumber':entryNumberController.text,
+        'entryStrength':entryStrengthController.text,
+        'entryTitle': entryTitleController.text,
+        'entrySlogan':entrySloganController.text,
+        'startDate': selectedStartDate != null
+            ? Timestamp.fromDate(selectedStartDate!)
+            : Timestamp.fromDate(DateTime.now()),
+ 
+      };
+
+      try {
+        FirebaseFirestore.instance
+            .collection('entrys')
+            .doc(entryNameController.text)
+            .set(entryData);
+      } catch (e) {
+        print(e);
+      }
     }
   }
 }
