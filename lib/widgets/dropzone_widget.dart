da35adaca0dbcbf2e028da7d142e53fa5c1c7b12
IHK_Office_Laptop <IHK_Office_Laptop>
2024-07-27 07:09:18 +0500

    dropping the Stepper idea altogether attempting to simplify the entrycreation form into one large single form

--- lib/widgets/dropzone_widget.dart
+++ lib/widgets/dropzone_widget.dart
@@ -12,59 +12,74 @@ class DropzoneWidget extends ConsumerStatefulWidget {
 
 class _DropzoneWidgetState extends ConsumerState<DropzoneWidget> {
   late DropzoneViewController controller;
-@override
- 
-
-
+  @override
   @override
   Widget build(BuildContext context) {
-    // controller= DropzoneViewController().;
     return Container(
       color: Colors.green.shade300,
       child: Center(
         child: Stack(
           children: [
-            DropzoneView(
-                operation: DragOperation.copy,
-                cursor: CursorType.copy,
-
-                onCreated: (controller) => this.controller = controller,
-                onDrop: onFileDrop),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const FaIcon(
-                    FontAwesomeIcons.cloudArrowUp,
-                    size: 60,
-                    color: Colors.white,
-                  ),
-                  const Text('Drop an Excel file here'),
-                  const Text('or'),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  ElevatedButton.icon(
-                    label: const Text('Choose File'),
-                    icon: const Icon(Icons.search),
-                    onPressed: () async {
-                      final events = await controller.pickFiles();
-                      if (events != null) {
-                        // Process the selected files
-                        for (final event in events) {
-                          final fileName = event.name;
-                          print('Selected File: $fileName');
-                          // You can access the file path using event.path
-                        }
-                      }
-                    },
-                  ),
-                ],
-              ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 16,
+              child: filePickButton(),
             ),
+
+            dropTarget(),
+            dropTargetHint(),
           ],
         ),
+      ),
+    );
+  }
+
+  Center dropTargetHint() {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          FaIcon(
+            FontAwesomeIcons.cloudArrowUp,
+            size: 60,
+            color: Colors.white,
+          ),
+          Text('Drop an Excel file here'),
+          Text('or'),
+          SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+
+  DropzoneView dropTarget() {
+    return DropzoneView(
+        operation: DragOperation.copy,
+        // cursor: CursorType.copy,
+        onCreated: (controller) => this.controller = controller,
+        onDrop: onFileDrop);
+  }
+
+  Widget filePickButton() {
+    return Center(
+      child: ElevatedButton.icon(
+        label: const Text('Choose File'),
+        icon: const Icon(Icons.search),
+        onPressed: () async {
+          final events = await controller.pickFiles();
+          if (events != null) {
+            // Process the selected files
+            for (final event in events) {
+              final fileName = event.name;
+              print('Selected File: $fileName');
+              // You can access the file path using event.path
+            }
+          }
+        },
       ),
     );
   }
