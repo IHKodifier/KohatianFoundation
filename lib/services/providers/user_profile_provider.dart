0ae4aa3ff9392070cd07f0c507866c2239a43a9a
IHK_Office_Laptop <IHK_Office_Laptop>
2024-08-11 08:42:23 +0500

    attempting to fix user not found om first login

--- lib/services/providers/user_profile_provider.dart
+++ lib/services/providers/user_profile_provider.dart
@@ -1,57 +1,8 @@
-//Provider
-// Provider for the AppUserNotifier
-// ignore_for_file: dead_code
+
 
 import 'package:kohatian_foundation/widget_export.dart';
 
-// final appUserProvider = StateNotifierProvider<AppUserNotifier, UserProfile?>(
-//     (ref) => AppUserNotifier());
 
-// //AppUserNotifier
-// class AppUserNotifier extends StateNotifier<UserProfile?> {
-//   AppUserNotifier() : super(null) {
-//     _init();
-//   }
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-//   Future<void> _init() async {
-//     // Check for initial authenticated user
-//     final user = _auth.currentUser;
-//     if (user != null) {
-//       await _fetchAndSetState(user);
-//     }
-//     // Listen for auth state changes
-//     _auth.authStateChanges().listen((user) async {
-//       if (user != null) {
-//         await _fetchAndSetState(user);
-//       } else {
-//         state = null;
-//       }
-//     });
-//   }
-// void signOut() async {
-//   await _auth.signOut();
-//   state = null;
-// }
-
-//   Future<void> _fetchAndSetState(User user) async {
-//     try {
-//       final doc = await _firestore.collection('users').doc(user.uid).get();
-//       if (doc.exists) {
-//         state = UserProfile.fromMap(doc.data()!);
-//       } else {
-//         // Handle user not found in Firestore
-//         // You might want to log an error or display an appropriate message
-//         print('User not found in Firestore');
-//       }
-//     } catch (e) {
-//       // Handle errors fetching user data
-//       // You might want to log the error or display an appropriate error message
-//       print('Error fetching user data: $e');
-//     }
-//   }
-// }
 
 final authStateChangesProvider = StreamProvider<User?>((ref) {
   return FirebaseAuth.instance.authStateChanges();
@@ -78,7 +29,7 @@ final userProfileProvider = StreamProvider<UserProfile?>((ref) {
       }
     });
   } else {
-    throw Exception('User not found');
+    throw Exception('Userrr not found');
     return Stream<UserProfile?>.value(null);
   }
 });
