ff6ce1838cd7f05d261e294a30f4123c9a006e67
IHK_Office_Laptop <IHK_Office_Laptop>
2024-09-15 20:36:42 +0500

    mobile sized grid implemented. cadet detail profile pending.

--- lib/pages/pearls_page.dart
+++ lib/pages/pearls_page.dart
@@ -5,10 +5,12 @@ import 'package:kohatian_foundation/widget_export.dart';
 import 'package:kohatian_foundation/widgets/entrry_picker.dart';
 
 class PearlsPage extends ConsumerWidget {
+  late BuildContext _context;
   PearlsPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-  final selectedEntry = ref.watch(selectedEntryProvider);
+    _context = context;
+    final selectedEntry = ref.watch(selectedEntryProvider);
     final cadetsAsync = ref.watch(cadetStreamProvider(selectedEntry));
     return Scaffold(
       body: CustomScrollView(
@@ -28,24 +30,24 @@ class PearlsPage extends ConsumerWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(12),
                         child: Card(
-                          elevation: 5,
-                          margin: const EdgeInsets.all(8),
-                          child:ref.watch(selectedEntryProvider)!=''?
-                          cadetsAsync.when(
-                            data: onCadetGridData,
-                              error: (error, stackTrace) => Text(
-                                    error.toString() + stackTrace.toString()),
-                             loading: () => const Center(
-                                  child: CircularProgressIndicator()),
-                             ):Container()
-                        ),
+                            elevation: 5,
+                            margin: const EdgeInsets.all(8),
+                            child: ref.watch(selectedEntryProvider) != ''
+                                ? cadetsAsync.when(
+                                    data: onCadetGridData,
+                                    error: (error, stackTrace) => Text(
+                                        error.toString() +
+                                            stackTrace.toString()),
+                                    loading: () => const Center(
+                                        child: CircularProgressIndicator()),
+                                  )
+                                : Container()),
                       ),
-                      ),
-                    
+                    ),
                   ],
                 )),
           ),
-           SliverToBoxAdapter(
+          SliverToBoxAdapter(
             child: SiteFooter(),
           )
         ],
@@ -55,22 +57,17 @@ class PearlsPage extends ConsumerWidget {
 
   Widget? onCadetGridData(List<Cadet> data) {
     return GridView.builder(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount:
-                                  4, // Adjust the number of columns as needed
-                              crossAxisSpacing: 16,
-                              mainAxisSpacing: 4,
-                              childAspectRatio:
-                                  0.95, // Adjust the aspect ratio as needed
-                            ),
-                            itemCount:  data.length,
-                             
-                            itemBuilder: (context, index) {
-                              return 
-                                    CadetGridTile(cadet: data[index]);
-                               
-                            },
-                          );
+      gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: ResponsiveBreakpoints.of(_context).largerThan(TABLET)?4:
+        2, // Adjust the number of columns as needed
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 12,
+        childAspectRatio: 0.8, // Adjust the aspect ratio as needed
+      ),
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return CadetGridTile(cadet: data[index]);
+      },
+    );
   }
 }
