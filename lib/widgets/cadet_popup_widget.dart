ccf24f369aee5cff4b5ad9351610f5ccbe2fb249
IHK_Office_Laptop <IHK_Office_Laptop>
2024-09-19 07:23:48 +0500

    layoutBuilder introduced on CadetPopupWidget but profiler image still not centerered

--- lib/widgets/cadet_popup_widget.dart
+++ lib/widgets/cadet_popup_widget.dart
@@ -3,59 +3,67 @@ import 'package:flutter/material.dart';
 import 'package:kohatian_foundation/widget_export.dart';
 
 class CadetPopupWidget extends StatelessWidget {
-
   final Cadet cadet;
 
-  const CadetPopupWidget({Key? key, required , required this.cadet}) : super(key: key);
+  const CadetPopupWidget({super.key, required, required this.cadet});
 
-@override
+  @override
   Widget build(BuildContext context) {
-    return Center(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final maxWidth = constraints.maxWidth;
+        final maxHeight = constraints.maxHeight;
+
+        return Center(
       child: Card(
         child: Column(
           children: [
             Stack(
               clipBehavior: Clip.none,
               children: [
-             buildBannerImage(),
-   Positioned(
-    top: 50,
-    left:50,
-    right: 50,
-    child: CircleAvatar( 
-      // radius: 72, 
-      minRadius: 52,
-      maxRadius: 52,
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      
-      child: Center(
-        child: CachedNetworkImage(imageUrl: 
-        cadet?.profileImageUrl?? 'https://picsum.photos/300/300',
-        fadeInDuration: Durations.medium4,
-        fadeInCurve: Curves.easeInCirc,
-        height: 100,
-        fit: BoxFit.contain,
-        
-        ),
-      ),
-    ),
-  )
+                    buildBannerImage(),
+                    Positioned(
+                      top: 50,
+                      left: (maxWidth / 2) - 50,
+                      right: (maxWidth / 2) - 50,
+                      child: CircleAvatar(
+                        // radius: 72,
+                        minRadius: 52,
+                        maxRadius: 52,
+                        backgroundColor: Theme.of(context).colorScheme.surface,
 
-            ],),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(cadet.name),
+                        child: Center(
+                          child: CachedNetworkImage(
+                            imageUrl: cadet.profileImageUrl ??
+                                'https://picsum.photos/300/300',
+                            fadeInDuration: Durations.medium4,
+                            fadeInCurve: Curves.easeInCirc,
+                            height: 100,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(cadet.name),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 
-CachedNetworkImage buildBannerImage() {
-  return CachedNetworkImage(imageUrl: cadet?.bannerImageUrl ?? 'https://cbx-prod.b-cdn.net/COLOURBOX52709581.jpg?width=800&height=800&quality=70',
-fit: BoxFit.contain,
-height: 100,  
- );
-}
+  CachedNetworkImage buildBannerImage() {
+    return CachedNetworkImage(
+      imageUrl: cadet.bannerImageUrl ??
+          'https://cbx-prod.b-cdn.net/COLOURBOX52709581.jpg?width=800&height=800&quality=70',
+      fit: BoxFit.fitWidth,
+      height: 100,
+    );
+  }
 }
